82c0bb1402375009c79329f32ee484b145572806
BrandonMorenoBalboa <36392312+BrandonMorenoBalboa@users.noreply.github.com>
2020-08-23 12:24:54 -0400

    Add files via upload

---
+++ view/footer.php
@@ -0,0 +1,44 @@
+    <!-- 
+        Email de contacto
+        hugovargas1998@gmail.com
+        Preparar Footer
+    -->
+    <footer class="border-top">
+        <div class="container">
+            <div class="row pt-2">
+                <div class="col-md-4">
+                    <img src="./img/logo.jpg" alt="" width="220px">
+                </div>
+                <div class="col-md-4">
+                    <h4 class="border-bottom">Contacto</h4>
+                    <ul>
+                        <li class=""><a class="text-body text-decoration-none" href="tel:+56987245189">Fono: +56 9 8724 5189</a></li>
+                        <li class=""><a class="text-body text-decoration-none" href="mailto:contacto@serviciosmineroshj.cl">Email: contacto<span class="text-warning">@</span>serviciosmineroshj.cl</a></li>
+                    </ul>
+                </div>
+                <div class="col-md-4">
+                    <h4 class="border-bottom">Redes sociales</h4>
+                    <ul>
+                        <li><a class="text-body text-decoration-none" href="" target="_blank"><i class="fab fa-facebook-square fa-1x"></i> Facebook</a></li>
+                        <li><a class="text-body text-decoration-none" href="" target="_blank"><i class="fab fa-instagram-square fa-1x"></i> Instagram</a></li>
+                        <li><a class="text-body text-decoration-none" href="" target="_blank"><i class="fab fa-linkedin fa-1x"></i> Linkedin</a></li>
+                        <li><a class="text-body text-decoration-none" href="https://wa.me/56987245189" target="_blank"><i class="fab fa-whatsapp-square fa-1x"></i> WhatsApp</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </footer>
+    <footer class="footer mt-auto pb-1 pt-1 bg-dark">
+        <div class="container">
+            <p class="text-white text-center">&copy <span id="anio"></span> Servicios Mineros</p>
+        </div>
+    </footer>
+    <script src="./js/jquery.min.js"></script>
+    <script src="./js/popper.min.js"></script>
+    <script src="./js/bootstrap.min.js"></script>
+    <script src="./js/all.js"></script>
+    <script src="./js/scripts.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/gh/fancyapps/fancybox@3.5.7/dist/jquery.fancybox.min.js"></script>
+</body>
+</html>
---
+++ view/header.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <!-- OG -->
+    <meta property="og:url" content="https://serviciosmineroshj.cl/">
+    <meta property="og:type" content="website">
+    <meta property="og:title" content="Servicios Mineros H-J">
+    <meta property="og:description" content="Brindamos una amplia gama de soluciones a nuestros clientes.">
+    <meta property="og:image" content="https://serviciosmineroshj.cl/img/background-op.png">
+
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./css/styles.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/meyer-reset/2.0/reset.min.css">
+    <link rel="stylesheet" href="./css/bootstrap.min.css">
+    <link rel="stylesheet" href="./css/all.css">
+    <link href="https://fonts.googleapis.com/css2?family=Staatliches&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/fancyapps/fancybox@3.5.7/dist/jquery.fancybox.min.css" />
+    <link rel="shortcut icon" href="./img/favicon.png" type="image/x-icon">
+    <title>Servicios Mineros</title>
+    
+</head>
+<body>
+    <header class="navbar-fixed">
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark pt-3 pb-3">
+            <a class="navbar-brand" href="#">
+                <img src="./img/logo-min.png" width="" height="30" alt="" loading="lazy">
+            </a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar" aria-controls="navbar" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbar">
+                <ul class="navbar-nav mr-auto">
+                    <li class="nav-item active">
+                        <a class="nav-link" href="#home">Inicio</a>
+                    </li>
+                    <li class="nav-item active">
+                        <a class="nav-link" href="#somos">Nosotros</a>
+                    </li>
+                    <li class="nav-item active">
+                        <a class="nav-link" href="#servicios">Servicios</a>
+                    </li>
+                    <li class="nav-item active">
+                        <a class="nav-link" href="#trabajos">Trabajos</a>
+                    </li>
+                    <li class="nav-item active">
+                        <a class="nav-link" href="#contacto">Contacto</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    </header>
